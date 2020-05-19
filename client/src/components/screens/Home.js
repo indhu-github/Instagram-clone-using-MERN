f04cff62944d9b5411093f10e6b8fd426ecc949c
indhu-github <indhusworkmail@gmail.com>
2020-05-19 10:57:34 +0530

    Update Home.js

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
       .then((res) => res.json())
       .then((result) => {
         console.log(result);
-        //setData(result.posts);
+        setData(result.posts);
       });
   }, []);
   return (
